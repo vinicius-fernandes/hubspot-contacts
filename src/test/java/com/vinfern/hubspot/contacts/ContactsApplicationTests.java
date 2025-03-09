a048f04512ce21c7aadc827f095f16092eb826af
vfernandes <viniciusnandes@hotmail.com>
2025-03-08 22:42:51 -0300

    Set context load test profile to test

--- src/test/java/com/vinfern/hubspot/contacts/ContactsApplicationTests.java
+++ src/test/java/com/vinfern/hubspot/contacts/ContactsApplicationTests.java
@@ -2,8 +2,11 @@ package com.vinfern.hubspot.contacts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ContactsApplicationTests {
 
 	@Test
